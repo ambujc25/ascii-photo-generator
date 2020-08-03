7d6f938c6f8275b970c7b898d59007f6e6dbdf95
ambujc <ambuj2122@gmail.com>
2020-08-03 11:37:19 +0530

    Working ASCII generator

---
+++ source.py
@@ -0,0 +1,42 @@
+from PIL import Image
+import math
+
+im = Image.open("Earth.jpg")
+print(im.size)
+
+#im = im.resize((int(0.02*im.size[0]),int(0.02*im.size[1])))
+im = im.resize((40,40))
+
+rows,cols = (im.size[0],im.size[1])
+print(rows,cols)
+arr = [[0]*cols for i in range(rows)]
+
+arr = list(im.getdata())
+arr = [arr[i*cols: (i+1)*cols] for i in range(rows)]
+
+brightness_matrix = [[0]*cols for i in range(rows)]
+
+i = 0;
+#for col in range(cols):
+	#brightness_matrix[i][col] = int((arr[i*cols+col][0] + arr[i*cols + col][1] + arr[i*cols + col][2])/3);
+
+for row in range(rows):
+	for col in range(cols):
+		#brightness_matrix[row][col] = math.floor((arr[row][col][0] + arr[row][col][1] + arr[row][col][2])/3)
+		#brightness_matrix[row][col] = 0.21*arr[row][col][0] + 0.72*arr[row][col][1] + 0.07*arr[row][col][2]
+		brightness_matrix[row][col] = int((max(arr[row][col][0],arr[row][col][1],arr[row][col][2]) + min(arr[row][col][0],arr[row][col][1],arr[row][col][2]))/2)
+		#print(int((arr[row][col][0]+arr[row][col][1]+arr[row][col][2])/3),end = " ")
+
+ascii_chars = "`^,:;Il!i~+_-?][}{1)(|/tfjrxnuvczXYUJCLQ0OZmwqpdbkhao*#MW&8@$"
+divider = 255/(len(ascii_chars)-1)
+
+ascii_matrix = [[0]*cols for i in range(rows)]
+for row in range(rows):
+	for col in range(cols):
+		ascii_matrix[row][col] = ascii_chars[math.floor(brightness_matrix[row][col]/divider)]
+
+for row in range(rows):
+	for col in range(cols):
+		for p in range(2):
+			print(ascii_matrix[row][col],end = "")
+	print()
